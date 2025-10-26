6085930f29a6f482511bbd577e1680a278c5afe4
Natxo <natxo.cabre@gmail.com>
2025-10-26 12:03:19 +0100

    chore: bump version to 1.0.18 [version bump]

--- js/version.js
+++ js/version.js
@@ -2,9 +2,9 @@
 // This is the single source of truth for version information
 
 export const VERSION = {
-  app: '1.0.17',
+  app: '1.0.18',
   buildDate: '2025-10-26',
-  gitCommit: '62809e9'
+  gitCommit: '1ae1611'
 };
 
 // Generate cache name for service worker
