f4db264e3077e0e588385afabc1c94b9cf990033
Natxo <natxo.cabre@gmail.com>
2025-10-26 09:50:36 +0100

    automatic bump version when pushing to remote/main

--- js/version.js
+++ js/version.js
@@ -2,9 +2,9 @@
 // This is the single source of truth for version information
 
 export const VERSION = {
-  app: '1.0.7',
+  app: '1.0.9',
   buildDate: '2025-10-26',
-  gitCommit: 'b369e03'
+  gitCommit: '2be96ce'
 };
 
 // Generate cache name for service worker
