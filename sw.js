baa0bb13e2e0a6ba6c79132dfa375810882b84a1
Natxo <natxo.cabre@gmail.com>
2025-10-31 11:24:02 +0100

    release: 0.0.69

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Service Worker for Mots - Offline-first PWA
 // Version is automatically read from package.json
 
-const VERSION = '0.0.68';
+const VERSION = '0.0.69';
 const CACHE_NAME = `mots-v${VERSION}`;
 
 // Error handler for unhandled errors
