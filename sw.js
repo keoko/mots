2d63a6096f168b2653a4d5fa6891ca6c7e2c42e6
Natxo <natxo.cabre@gmail.com>
2025-11-17 07:40:11 +0100

    release: 0.0.105

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // This ensures the service worker cache is properly invalidated
 // The app.js registration uses ?v=X.X.X query parameter to force browser to check for updates
 
-const VERSION = '0.0.104';
+const VERSION = '0.0.105';
 const CACHE_NAME = `mots-v${VERSION}`;
 const ASSETS_TO_CACHE = [
   './',
